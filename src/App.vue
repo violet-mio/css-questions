b85cc80ddee4bb91a5b46a8c0269702d76151290
violet-mio <caohai648@qq.com>
2023-02-11 16:57:58 +0800

    feat: 新增嵌套路由

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,65 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { routes } from './router/index'
+import { useRoute } from 'vue-router'
+import { computed, watch } from 'vue'
+
+const route = useRoute()
+const parentRoute = computed(() => {
+  // 获取路由记录
+  const matched = route.matched
+
+  // 获取该记录的路由个数
+  const max = matched.length
+
+  // 获取倒数第二个路由（也就是当前路由的父级路由）
+   const res = matched[max - 2]
+   console.log('res', res)
+   return res || []
+})
+
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink class="link" to="/height-question/way">方式1：父元素设置`overflow:hidden`</RouterLink>
-        <RouterLink class="link" to="/height-question/way2">方式2: 父元素内的末尾新增空元素，并设置清除浮动属性</RouterLink>
-        <RouterLink class="link" to="/height-question/way3">方式3：父元素设置浮动</RouterLink>
-        <RouterLink class="link" to="/height-question/way4">方式4：父元素末尾新增伪元素并设置`clear:both`</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <div class="nav">
+    <RouterLink class="link" v-for="item in routes" :to="item.path" :key="item.path">
+      {{ item.name || item.path }} 
+    </RouterLink>
+  </div>
+  <div class="wrapper">
+    <RouterLink class="link" v-for="child in parentRoute.children" :to="child.path" :key="child.path">
+      {{ child?.meta?.desc }} 
+    </RouterLink>
+  </div>
 
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-nav {
-  width: 90vw;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-.link {
-  display: block;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<style lang="scss" scoped>
+.nav {
+  margin-bottom: 20px;
+  .link {
+    padding: 6px 10px;
+    border: 1px solid skyblue;
+    margin-left: 10px;
+    &:first-child {
+      margin-left: 0;
+    }
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .router-link-active.link {
+    color: #ca6700;
   }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  .link:not(.router-link-active) {
+    color: black;
   }
 }
+
+.wrapper {
+  .link {
+    display: block;
+  }
+}
+
+
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
+export const routes = [
     {
       path: '/',
       name: 'home',
@@ -12,25 +10,45 @@ const router = createRouter({
           path: '/height-question/way',
           name: 'way',
           component: () => import('../views/height-question/Way.vue'),
+          meta: { desc: '方式1：父元素设置`overflow:hidden`' }
         },
         {
           path: '/height-question/way2',
           name: 'way2',
-          component: () => import('../views/height-question/Way2.vue')
+          component: () => import('../views/height-question/Way2.vue'),
+          meta: { desc: '方式2: 父元素内的末尾新增空元素，并设置清除浮动属性' }
         },
         {
           path: '/height-question/way3',
           name: 'way3',
-          component: () => import('../views/height-question/Way3.vue')
+          component: () => import('../views/height-question/Way3.vue'),
+          meta: { desc: '方式3：父元素设置浮动' }
         },
         {
           path: '/height-question/way4',
           name: 'way4',
-          component: () => import('../views/height-question/Way4.vue')
+          component: () => import('../views/height-question/Way4.vue'),
+          meta: { desc: '方式4：父元素末尾新增伪元素并设置`clear:both`' }
+        }
+      ]
+    },
+    {
+      path: '/bfc',
+      name: 'bfc',
+      redirect: { name: 'marginMerge' },
+      children: [
+        {
+          path: '/bfc/margin-merge',
+          name: 'marginMerge',
+          component: () => import('../views/bfc/index.vue'),
         }
       ]
     },
   ]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: routes
 })
 
 export default router
